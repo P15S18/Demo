02afe5fd527fd79fa40da2673f7395182ca55a8d
P15S18 <pkmaurya1509@gmail.com>
2021-12-16 12:55:37 +0530

    Java file

--- test.java
+++ test.java
@@ -1,7 +1,16 @@
-class java
+class Simple
 {
+    public void add(int a , int b)
+    {
+
+        int c;
+        c= a+b;
+        System.out.println("Additin of 2 no", c);
+    }
     public static void main(String args[])
     {
+        Simple s1 = new Simple ();
+        s1.add(10,20);
         System.out.println("my test");
     }
 
