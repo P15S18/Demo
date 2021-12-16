95526465934e98e47fade6875d54fd5421bb7a99
P15S18 <pkmaurya1509@gmail.com>
2021-12-16 12:57:32 +0530

    Resolve merge conflict

--- test.java
+++ test.java
@@ -2,6 +2,12 @@ class java
 {
     public static void main(String args[])
     {
+
+
+        Simple s1 = new Simple ();
+        s1.add(10,20);
+        s1.add(18,20);
+
         System.out.println("my test");
     }
 
