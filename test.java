e1a2017223d3721e1dc91fe30b71349e9f2ce2ad
P15S18 <pkmaurya1509@gmail.com>
2021-12-16 13:10:02 +0530

    Reset

--- test.java
+++ test.java
@@ -7,7 +7,10 @@ class java
         Simple s1 = new Simple ();
         s1.add(10,20);
         s1.add(18,20);
+<<<<<<< HEAD
 
+=======
+>>>>>>> cd186ef (Modify java file)
         System.out.println("my test");
     }
 
